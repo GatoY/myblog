0bb3417ebe8756c0eb664ae18a27b2b3f060a621
GatoY <derakhy@gmail.com>
2017-09-01 22:57:57 +1000

    set markdown and highlight

--- apps/admin.py
+++ apps/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category, Tag
 admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Tag)
 # Register your models here.
--- apps/views.py
+++ apps/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import get_object_or_404,render
 from django.http import Http404,HttpResponse
 from .models import Article
-
+import markdown
+import pygments
 def index(request):
     article_list=Article.objects.order_by('-cre_date')
     context={'article_list':article_list}
     return render(request,'apps/index.html',context)
 def detail(request,article_id):
+    article=get_object_or_404(Article, pk=article_id)
     article.views+=1
     article.save()
-    article=get_object_or_404(Article, pk=article_id)
+    article.body=markdown.markdown(article.body,
+				    extensions=[
+				        'markdown.extensions.extra',
+					'markdown.extensions.codehilite',
+					'markdown.extensions.toc',
+				    ])
     return render(request,'apps/detail.html',{'article':article})
