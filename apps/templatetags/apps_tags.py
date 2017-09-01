63e105628be730b4d241d2b544997d9e839af375
GatoY <derakhy@gmail.com>
2017-09-01 23:53:33 +1000

    add tags, not done

---
+++ apps/templatetags/apps_tags.py
@@ -0,0 +1,17 @@
+from ..models import Article, Category
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def get_recent_article(num=5):
+    return Article.objects.all().order_by('cre_date')[:num]
+
+@register.simple_tag
+def archives():
+    return Article.objects.dates('cre_date','month',order='DESC')
+
+@register.simple_tag
+def get_categories():
+    return Category.objects.all()
+
