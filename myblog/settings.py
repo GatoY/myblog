8c0edb4d07cbee100503246cfffe371e4784e515
GatoY <derakhy@gmail.com>
2017-10-01 23:52:41 +1100

    good version

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mn)3zi97w4%2(5uccme2bx=bnkfd2j8036z_xkn-4ot^28%&(7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.derakhy.com']
 
